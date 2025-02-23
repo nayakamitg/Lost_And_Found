d8b086abac2c4d02fdcd565673e3d0d2f748bdad
Amit kumar <103359090+nayakamitg@users.noreply.github.com>
2025-02-23 13:55:41 +0530

    Add files via upload

---
+++ Home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Home.models import Person,Mails
+
+# Register your models here.
+
+
+admin.site.register(Person)
+admin.site.register(Mails)
---
+++ Home/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from Home import views
+
+urlpatterns = [
+    path('', views.home,name="home"),
+    path('add_person/', views.add_person,name="add_person" ),
+    path('details/<int:sno>/', views.detail,name="Details" ),
+]
