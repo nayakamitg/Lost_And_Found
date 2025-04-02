e971b4ee7ace189c9f4f605add8729f01a9f1c99
Amit kumar <103359090+nayakamitg@users.noreply.github.com>
2025-04-03 00:35:33 +0530

    Update settings.py

--- Lost_and_Found1/settings.py
+++ Lost_and_Found1/settings.py
@@ -116,20 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collects static files here
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Collects static files here
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 import os
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
